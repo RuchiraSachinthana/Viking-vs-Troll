e0a0d40d1a986cc69a7e131236f77413603a3a40
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-04-06 16:46:33 +0530

    updated

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 var audio = new Audio("./assets/sounds/enterGame.wav");
+var mainAudio = new Audio("./assets/sounds/mainMusic.mp3");
 
 function buttonClick() {
     audio.play();
+    mainMusic();
+}
+
+function mainMusic(){
+    mainAudio.play();
+    mainAudio.loop = true;
+}
+
+function pauseMainAudio(){
+    mainAudio.pause();
 }
