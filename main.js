4f6ee048aec56439556cd6614713156645e2c11a
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-04-18 22:05:49 +0530

    updated

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 var audio = new Audio("./assets/sounds/enterGame.wav");
 var mainAudio = new Audio("./assets/sounds/mainMusic.mp3");
+var viking = document.getElementById("viking");
+var troll = document.getElementById("troll");
+
+
+
 
 function buttonClick() {
     audio.play();
@@ -13,4 +18,14 @@ function mainMusic(){
 
 function pauseMainAudio(){
     mainAudio.pause();
+}
+
+function removeAll() {
+    var name = document.getElementById("name");
+    var playBtn = document.getElementById("play-btn");
+    var footer = document.getElementById("footer");
+    name.style.visibility = "hidden";
+    playBtn.style.visibility = "hidden";
+    footer.style.visibility = "hidden";
+
 }
